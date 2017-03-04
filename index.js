7eeb13e5b5757c21684d8f4121b8a91cd7f526da
John Zeiders <zeiders@usc.edu>
2017-03-04 01:41:14 -0800

    Test

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express'),
     app = express(),
     db = require('./db.js');
 
+app.get("/test", function(req,res){
+    res.send("TEST")
+})
+
 app.listen(8888, function(){
     console.log("WE GOT A SERVER")
 });
